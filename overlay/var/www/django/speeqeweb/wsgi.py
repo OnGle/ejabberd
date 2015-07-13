7ac82789f4c18650d4280afbeddcf519ae53fca8
OnGle <nafets.sivad@gmail.com>
2015-07-13 01:03:09 +0000

    Revert "Big dirty commit"
    This reverts commit c569e1c28063334c2bb77acee696829d07511c10.

--- overlay/var/www/django/speeqeweb/wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/..')
-sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../speeqeweb')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "speeqeweb.settings")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
